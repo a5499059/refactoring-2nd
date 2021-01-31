43fa1c07f82411868a07ffc594d76f86c46795a3
are99059 <a5499059@gmail.com>
2021-02-01 00:59:17 +0900

    move place of usd method

--- src/statement.ts
+++ src/statement.ts
@@ -5,19 +5,7 @@ export function statement(invoice: Invoice, plays: Plays): string {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
-export function usd(aNumber: number): string {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
-}
-
 export function renderPlainText(data): string {
-  //fuction statement
-
-  //top level
-
   let result: string = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {
@@ -30,4 +18,12 @@ export function renderPlainText(data): string {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
+
+export function usd(aNumber: number): string {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
