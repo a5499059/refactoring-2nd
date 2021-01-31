da750c7c6d72ca34f3b2146ac92d484cd754aaa8
are99059 <a5499059@gmail.com>
2021-02-01 00:58:33 +0900

    add StatemantData type

--- src/types/allTypes.ts
+++ src/types/allTypes.ts
@@ -1,18 +1,24 @@
 export type Performance = {
-    playID: string;
-    audience: number;
-  }
-  
-  export type Invoice = {
-    customer: string;
-    performances: Array<Performance>;
-  }
-  
-  export type Play = {
-    name: string;
-    type: string;
-  }
+  playID: string;
+  audience: number;
+};
 
-  export type Plays = {
-    [playID : string]:Play
-  }
+export type Invoice = {
+  customer: string;
+  performances: Array<Performance>;
+};
+
+export type Play = {
+  name: string;
+  type: string;
+};
+
+export type Plays = {
+  [playID: string]: Play;
+};
+
+export type StatementData = Performance & {
+  play?: Play;
+  amount?: number;
+  volumeCredits?: number;
+};
