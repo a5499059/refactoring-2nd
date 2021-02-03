aa8cd08bd679abe01b115bb93ba3c8023b4b6f79
are99059 <a5499059@gmail.com>
2021-02-04 01:42:09 +0900

    copy amountFor method from createStatemenet.ts to PerformanceCaliculator

--- src/PerformanceCalculator.ts
+++ src/PerformanceCalculator.ts
@@ -7,4 +7,26 @@ export class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = play;
   }
+   //function statement
+   get amount(): number {
+    let result: number = 0;
+    switch (this.play.type) {
+      case "tragedy": //悲劇ならまずは40,000円で３０人まで。追加分一人あたり１０００円
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30); //
+        }
+        break;
+      case "comedy": //喜劇ならまずは30,000円＋一人あたり３００円（ただし２０人まで）。２１人以上の場合は、追加10,000円と追加一人あたりさらに＋５００円。
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20); //
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  };
 }
